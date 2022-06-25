10f6df9c7368aa68abac65bc368e254ee8806316
adirx20 <87542990+adirx20@users.noreply.github.com>
2022-06-25 03:26:45 +0300

    bug fix

--- src/components/App.js
+++ src/components/App.js
@@ -46,7 +46,7 @@ function App() {
   const [selectedImage, setSelectedImage] = React.useState({});
 
   // Auth state variables
-  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [loggedIn, setLoggedIn] = React.useState(true);
 
   // Functions
   // Click handlres
@@ -127,15 +127,14 @@ function App() {
     <div className='page'>
 
       <Routes>
+        <Header
+          logo={logo}
+        />
+
         <ProtectedRoute
-          exact path='/'
+          path='/'
           loggedIn={loggedIn}
         >
-
-          <Header
-            logo={logo}
-          />
-
           <Main
             onEditProfileClick={handleEditProfileClick}
             onAddCardClick={handleAddCardClick}
@@ -146,63 +145,14 @@ function App() {
             onClose={closeAllPopups}
             isImagePopupOpen={isImagePopupOpen}
           />
-
-          <Footer />
-
-          <PopupWithForm name='edit-profile' title='Edit profile' saveButtonTitle='Save'
-            isOpen={isEditProfilePopupOpen}
-            onClose={closeAllPopups}
-          >
-            <input id='name-input' className='form__input form__input_type_name' type='text' name='name' placeholder='Name'
-              defaultValue='' required minLength='2' maxLength='40' />
-            <span id='name-input-error' className='form__input-error-message'></span>
-            <input id='proffession-input' className='form__input form__input_type_profession' type='text' name='profession'
-              placeholder='About me' defaultValue='' required minLength='2' maxLength='200' />
-            <span id='proffession-input-error' className='form__input-error-message'></span>
-          </PopupWithForm>
-
-          <PopupWithForm name='edit-avatar' title='Change profile picture' saveButtonTitle='Save'
-            isOpen={isEditAvatarPopupOpen}
-            onClose={closeAllPopups}
-          >
-            <input id='avatar-link-input' className='form__input form__input_type_avatar-link' type='url' name='avatar-link'
-              placeholder='Image URL' defaultValue='' required onChange={handleInput} />
-            <span id='avatar-link-input-error' className='form__input-error-message'></span>
-          </PopupWithForm>
-
-          <PopupWithForm name='add-card' title='New Place' saveButtonTitle='Save'
-            isOpen={isAddCardPopupOpen}
-            onClose={closeAllPopups}
-          >
-            <input id='card-input' className='form__input form__input_type_card-title' type='text' name='card-title'
-              placeholder='Title' defaultValue='' required minLength='1' maxLength='30' />
-            <span id='card-input-error' className='form__input-error-message'></span>
-            <input id='card-link-input' className='form__input form__input_type_card-link' type='url' name='card-link'
-              placeholder='Image URL' defaultValue='' required />
-            <span id='card-link-input-error' className='form__input-error-message'></span>
-          </PopupWithForm>
-
-          <PopupWithForm name='delete-card' title='Are you sure?' saveButtonTitle='Yes'
-            isOpen={isDeleteCardPopupOpen}
-            onClose={closeAllPopups}
-            handleSubmit={deleteCard} />
-
-          <ImagePopup
-            isOpen={isImagePopupOpen}
-            onClose={closeAllPopups}
-            imageLink={selectedImage.link}
-            imageCaption={selectedImage.name}
-          />
-
         </ProtectedRoute>
-
         <Route path='/signup'>
-          <Header
+          {/* <Header
             // loggedIn={isLoggedIn}
             userEmail={email}
             buttonText='Log in'
             url='/signin'
-          />
+          /> */}
           <Register
             title="Sign up"
             link="Log in"
@@ -211,20 +161,79 @@ function App() {
           />
         </Route>
         <Route path='/signin'>
-          <Header
+          {/* <Header
             // loggedIn={isLoggedIn}
             buttonText='Sign up'
             url='/signup'
-          />
+          /> */}
           <Login
             title="Log in"
             link='Sign up'
             loggedIn={setLoggedIn}
-            // onSubmit={handleLoginSubmit}
+          // onSubmit={handleLoginSubmit}
           />
         </Route>
       </Routes>
 
+      <Footer />
+
+      <PopupWithForm
+        name='edit-profile'
+        title='Edit profile'
+        saveButtonTitle='Save'
+        isOpen={isEditProfilePopupOpen}
+        onClose={closeAllPopups}
+      >
+        <input id='name-input' className='form__input form__input_type_name' type='text' name='name' placeholder='Name'
+          defaultValue='' required minLength='2' maxLength='40' />
+        <span id='name-input-error' className='form__input-error-message'></span>
+        <input id='proffession-input' className='form__input form__input_type_profession' type='text' name='profession'
+          placeholder='About me' defaultValue='' required minLength='2' maxLength='200' />
+        <span id='proffession-input-error' className='form__input-error-message'></span>
+      </PopupWithForm>
+
+      <PopupWithForm
+        name='edit-avatar'
+        title='Change profile picture'
+        saveButtonTitle='Save'
+        isOpen={isEditAvatarPopupOpen}
+        onClose={closeAllPopups}
+      >
+        <input id='avatar-link-input' className='form__input form__input_type_avatar-link' type='url' name='avatar-link'
+          placeholder='Image URL' defaultValue='' required onChange={handleInput} />
+        <span id='avatar-link-input-error' className='form__input-error-message'></span>
+      </PopupWithForm>
+
+      <PopupWithForm
+        name='add-card'
+        title='New Place'
+        saveButtonTitle='Save'
+        isOpen={isAddCardPopupOpen}
+        onClose={closeAllPopups}
+      >
+        <input id='card-input' className='form__input form__input_type_card-title' type='text' name='card-title'
+          placeholder='Title' defaultValue='' required minLength='1' maxLength='30' />
+        <span id='card-input-error' className='form__input-error-message'></span>
+        <input id='card-link-input' className='form__input form__input_type_card-link' type='url' name='card-link'
+          placeholder='Image URL' defaultValue='' required />
+        <span id='card-link-input-error' className='form__input-error-message'></span>
+      </PopupWithForm>
+
+      <PopupWithForm
+        name='delete-card'
+        title='Are you sure?'
+        saveButtonTitle='Yes'
+        isOpen={isDeleteCardPopupOpen}
+        onClose={closeAllPopups}
+        handleSubmit={deleteCard}
+      />
+
+      <ImagePopup
+        isOpen={isImagePopupOpen}
+        onClose={closeAllPopups}
+        imageLink={selectedImage.link}
+        imageCaption={selectedImage.name}
+      />
 
       <InfoTooltip
         name='success-tip'
